f7cd25d6f080d1fb41517fd0be421d5de7a69c64
Santamarian Bogdan <santamarian.bogdan@gmail.com>
2018-01-17 20:58:11 +0200

    added styling and slide toggle options

---
+++ CoinToss/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+    // Play against computer
+
+    $("#vs").on("click", function () {
+        $(".options").slideToggle("slow", function () {
+
+            var output = document.getElementById('output');
+            var userChoice = prompt('Head 1 or Tail 0 ?');
+            var computerChoice = Math.floor(Math.random() * 2);
+            console.log(computerChoice);
+
+            if (userChoice < computerChoice) {
+                output.innerHTML = computerChoice;
+                output.innerHTML = "You lose !";
+            } else if (userChoice == computerChoice) {
+                output.innerHTML = "Tie !"
+            } else {
+                output.innerHTML = "You win !"
+            }
+        });
+    });
+
+
+    // Guess what computer chose
+
+    $("#guess").on("click", function () {
+        $(".options").slideToggle("slow", function () {
+
+            var output = document.getElementById('output');
+            var userChoice = prompt('Heads or tails ?');
+            var computerChoice = Math.floor(Math.random() * 2);
+            console.log(computerChoice);
+
+            function result() {
+                if (userChoice == 'heads') {
+                    userChoice = 1;
+                } else {
+                    userChoice = 0;
+                }
+            };
+
+            result();
+
+            if (userChoice == computerChoice) {
+                output.innerHTML = "You were right !";
+            } else {
+                output.innerHTML = "You were wrong !";
+            }
+        });
+    });
+
+})
+
+
