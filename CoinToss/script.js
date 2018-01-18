b856b033e58a5edde828f1d87188f40aac80a46b
Santamarian Bogdan <santamarian.bogdan@gmail.com>
2018-01-18 10:54:24 +0200

    complete

--- CoinToss/script.js
+++ CoinToss/script.js
@@ -2,55 +2,60 @@ $(document).ready(function () {
 
     // Play against computer
 
-    $("#vs").on("click", function () {
-        $(".options").slideToggle("slow", function () {
-            $(this).toggleClass('expand').css('display', '');
+    // $("#vs").on("click", function () {
+    //     $(".options").slideToggle("slow", function () {
+    //         $(this).toggleClass('expand').css('display', '');
 
-            var output = document.getElementById('output');
-            var userChoice = prompt('Head 1 or Tail 0 ?');
-            var computerChoice = Math.floor(Math.random() * 2);
-            console.log(computerChoice);
-
-            if (userChoice < computerChoice) {
-                output.innerHTML = computerChoice;
-                output.innerHTML = "You lose !";
-            } else if (userChoice == computerChoice) {
-                output.innerHTML = "Tie !"
-            } else {
-                output.innerHTML = "You win !"
-            }
-        });
-    });
+    //         var output = document.getElementById('output');
+    //         var userChoice = "";
+    //         var computerChoice = Math.floor(Math.random() * 2);
+    //         console.log(computerChoice);
+            
+    //         $('.choice').on("click", function (){
+    //             if ( $(this).attr("id") == "heads") {
+    //                 userChoice = 1;
+    //             } else {
+    //                 userChoice = 0;
+    //             }
+                
+    //             if (userChoice < computerChoice) {
+    //                 output.innerHTML = computerChoice;
+    //                 output.innerHTML = "You lose !";
+    //             } else if (userChoice == computerChoice) {
+    //                 output.innerHTML = "Tie !"
+    //             } else {
+    //                 output.innerHTML = "You win !"
+    //             }
+    //         });   
+    //     });
+    // });
 
     // Guess what computer chose
-    
+
     $("#guess").on("click", function () {
         $(".options").slideToggle("slow", function () {
             $(this).toggleClass('expand').css('display', '');
 
             var output = document.getElementById('output');
-            var userChoice = prompt('Heads or tails ?');
-            var computerChoice = Math.floor(Math.random() * 2);
-            console.log(computerChoice);
+            var userChoice = "";
 
-            function result() {
-                if (userChoice == 'heads') {
+            $('.choice').on("click", function (){
+                var computerChoice = Math.floor(Math.random() * 2);
+                    
+                if ($(this).attr("id") == "heads") {
                     userChoice = 1;
                 } else {
                     userChoice = 0;
+                };
+
+                if (userChoice == computerChoice) {
+                    output.innerHTML = "You were right !";
+                } else {
+                    output.innerHTML = "You were wrong !";
                 }
-            };
-
-            result();
-
-            if (userChoice == computerChoice) {
-                output.innerHTML = "You were right !";
-            } else {
-                output.innerHTML = "You were wrong !";
-            }
+    
+            });            
         });
     });
 
-})
-
-
+});
